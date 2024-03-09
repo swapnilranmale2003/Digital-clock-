0fe7589faa9d3fc46ecac612099e04795638c331
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-03-09 22:40:51 +0530

    add date

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ setInterval(() => {
     const date = new Date();
     clock.innerHTML = date.toLocaleTimeString();    
 }, 1000);
+
+const days = document.getElementById('day');
+setTimeout(() => {
+    const d = new Date();
+    days.innerHTML = d.toDateString();
+    ;
+}, 1000);
+
+
